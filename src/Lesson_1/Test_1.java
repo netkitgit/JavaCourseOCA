2e38befac4754c8f20eedb54c94859ae271bb2d0
User Demogit <demo@demogit.com>
2019-03-27 16:41:47 +0300

    Work with class Test_1. Add HomeWork class

--- src/Lesson_1/Test_1.java
+++ src/Lesson_1/Test_1.java
@@ -1,4 +1,17 @@
 package Lesson_1;
 
 public class Test_1 {
+    public static void main(String[] args) {
+        System.out.print("Hello...");
+        System.out.println("Wellcome to first lesson!!!");
+        System.out.println();
+
+        System.out.print("A ");
+        System.out.println("a");
+        System.out.print("B ");
+        System.out.println("b");
+        System.out.print("C ");
+        System.out.println("c");
+
+    }
 }
